2512282ce54afa0a0a378911741a89d3ceb810a7
Daniel Achleitner <daniel.achleitner@gmail.com>
2017-01-14 22:00:21 +0100

    JOSM 11427

---
+++ josm/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'josm'
+  fileType      = 'EXE'
+  url           = 'https://josm.openstreetmap.de/download/windows/josm-setup-11427.exe'
+  checksum      = 'cc54c872aabb4ce9a2d154768434f3465e798df45569827e6f57df94a0da1602'
+  checksumType  = 'sha256'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ josm/tools/chocolateyuninstall.ps1
@@ -0,0 +1,33 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'josm'
+$softwareName   = 'JOSM *'
+$installerType  = 'EXE'
+$validExitCodes = @(0)
+$uninstalled    = $false
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $file = "$($_.UninstallString.Trim('"'))"
+    $instDir = "$(Split-Path $file)\"
+    $silentArgs = "_?=$instDir"
+
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+
+    Remove-Item "$file"
+    Remove-Item "$instDir"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
