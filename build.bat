b29bd8d67e1a9ba3992bdc5c33b8b068c05435f2
Daniel Achleitner <daniel.achleitner@gmail.com>
2017-10-30 20:19:25 +0100

    Build/install/push script

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+
+for %%a in (.) do set ID=%%~nxa
+choco pack %ID%.nuspec || echo Fail. && goto :quit
+
+for /f %%i in ('dir /b /a:-d /o:d /t:w %ID%.*.nupkg') do set NUPKG="%%i"
+choco upgrade %ID% -source "'.;https://chocolatey.org/api/v2/'" -y || echo Fail? && goto :quit
+
+:choice
+set /p push="Push %NUPKG% now? [y/n] "
+if /I "%push%" EQU "Y" goto :push
+if /I "%push%" EQU "N" goto :quit
+goto :choice
+
+:push
+choco push %NUPKG%
+
+:quit
