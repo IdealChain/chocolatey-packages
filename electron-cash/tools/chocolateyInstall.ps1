41dcfa0117dc38ddd5e4542316e61c373990d967
Daniel Achleitner <daniel.achleitner@gmail.com>
2018-06-25 02:42:09 +0200

    Electron Cash 3.3

--- electron-cash/tools/chocolateyInstall.ps1
+++ electron-cash/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $shortcutPath = "$(Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Prog
 $packageArgs = @{
     packageName    = 'electron-cash'
     fileFullPath   = $installFile
-    url            = 'https://electroncash.org/downloads/3.2/win-linux/ElectronCash-3.2.exe'
-    checksum       = '9696ed4c2aa6f9a6ecc2abcc3a0cb642da026ed72f990f6979c1b2d9c28d7d6e'
+    url            = 'https://electroncash.org/downloads/3.3/win-linux/ElectronCash-3.3.exe'
+    checksum       = '4d092f231970c1d87b9a0b65cebc1e201b2b9c94800734f1ecdcfe500f9b3d7e'
     checksumType   = 'sha256'
     forceDownload  = $true
 }
