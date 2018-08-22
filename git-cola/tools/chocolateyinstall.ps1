c5ce936daa864937b554222af7c501efe5f86893
Daniel Achleitner <daniel.achleitner@gmail.com>
2018-08-23 00:37:41 +0200

    Git Cola 3.1

---
+++ git-cola/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$tempDir = (Get-Item $env:TEMP).FullName
+
+$packageArgs = @{
+  packageName  = 'git-cola'
+  fileFullPath = (Join-Path $tempDir "git-cola.zip")
+  url          = 'https://github.com/git-cola/git-cola/releases/download/v3.1/git-cola-3.1.windows.zip'
+  checksum     = '6c9b0aab81a0ebbee42edf01483fc37d51f7cf3d230ec6ae1430fb056f5969dc'
+  checksumType = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+Get-ChocolateyUnzip @packageArgs -Destination $tempDir
+
+$installArgs = @{
+  packageName    = 'git-cola'
+  file           = (Join-Path $tempDir "git-cola-3.1.exe")
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @installArgs
+Remove-Item ($packageArgs['fileFullPath'], $installArgs['file'])
---
+++ git-cola/tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'git-cola'
+  softwareName   = 'git-cola'
+  installerType  = 'EXE'
+  validExitCodes = @(0)
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file']       = $_.UninstallString.Trim('"')
+    $packageArgs['instDir']    = "{0}\" -f $(Split-Path $packageArgs['file'])
+    $packageArgs['silentArgs'] = "/S _?={0}" -f $packageArgs['instDir']
+
+    Uninstall-ChocolateyPackage @packageArgs
+    Remove-Item ($packageArgs['file'], $packageArgs['instDir'])
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "{0} has already been uninstalled by other means." -f $packageArgs['packageName']
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
