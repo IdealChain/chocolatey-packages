055a2eb81f65d09bf58caeadd0e318dd83fdf410
Daniel Achleitner <daniel.achleitner@gmail.com>
2019-03-24 21:03:16 +0100

    Git Cola 3.3

--- git-cola/tools/chocolateyinstall.ps1
+++ git-cola/tools/chocolateyinstall.ps1
@@ -4,17 +4,18 @@ $tempDir = (Get-Item $env:TEMP).FullName
 $packageArgs = @{
   packageName  = 'git-cola'
   fileFullPath = (Join-Path $tempDir "git-cola.zip")
-  url          = 'https://github.com/git-cola/git-cola/releases/download/v3.2/git-cola-3.2.windows.zip'
-  checksum     = 'a278002161302e4d820ad1b21fca442ada4a1c300da7829875d7aa5c3b4febe3'
+  url          = 'https://github.com/git-cola/git-cola/releases/download/v3.3/git-cola-3.3.windows.zip'
+  checksum     = '3bb4e1cdfd01033b47b7fb691e0f4074fbc928366068cd7d6b77ee6e286ddfb6'
   checksumType = 'sha256'
 }
 
 Get-ChocolateyWebFile @packageArgs
 Get-ChocolateyUnzip @packageArgs -Destination $tempDir
 
+$installer = Get-ChildItem (Join-Path $tempDir "git-cola*.exe") | select -First 1
 $installArgs = @{
   packageName    = 'git-cola'
-  file           = (Join-Path $tempDir "git-cola-3.2.exe")
+  file           = $installer
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
