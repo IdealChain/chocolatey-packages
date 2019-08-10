5a546ae0c1cfbf17aadcb596296e844856ca1e4c
Daniel Achleitner <daniel.achleitner@gmail.com>
2019-08-10 13:47:34 +0200

    Electron Cash 4.0.9

--- electron-cash.install/tools/chocolateyinstall.ps1
+++ electron-cash.install/tools/chocolateyinstall.ps1
@@ -1,14 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-# migration: if exists, remove shortcut for old portable version
-$shortcutPath = "$(Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Electron Cash.lnk')"
-Remove-Item "$shortcutPath" -ErrorAction Ignore
-
 $packageArgs = @{
   packageName   = 'electron-cash'
   fileType      = 'EXE'
-  url           = 'https://electroncash.org/downloads/4.0.8/win-linux/Electron-Cash-4.0.8-setup.exe'
-  checksum      = '26787d90c99890392c14283c6c36ef8732ca1ae948eb1dcaded3366a99e030b3'
+  url           = 'https://electroncash.org/downloads/4.0.9/win-linux/Electron-Cash-4.0.9-setup.exe'
+  checksum      = '32045d756b4afa1f253951c18b0f50eebaf6b5b5830de22d80b990a136ea04b2'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
