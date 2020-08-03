9f0550bd48aa0f765f738bd56670012c01ed35e2
Daniel Achleitner <daniel.achleitner@gmail.com>
2020-08-04 01:43:42 +0200

    JOSM 16812

--- josm/tools/chocolateyinstall.ps1
+++ josm/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'josm'
   fileType      = 'EXE'
-  url           = 'https://josm.openstreetmap.de/download/windows/josm-setup-16731.exe'
-  checksum      = 'be43dafa38ebdded43f5cd32cd29b2fb4559e15ce11d178c65d549c0e63de458'
+  url           = 'https://josm.openstreetmap.de/download/windows/josm-setup-16812.exe'
+  checksum      = 'c64f4a5b1c40c8551b8a21a6bae5706580e21d80372de99dea3104b9ad3d19d6'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
