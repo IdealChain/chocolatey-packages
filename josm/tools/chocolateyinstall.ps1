dd658be779f60ac6a01717e6262f3dc1aa2c6585
Daniel Achleitner <daniel.achleitner@gmail.com>
2021-08-05 09:43:10 +0200

    JOSM 18118

--- josm/tools/chocolateyinstall.ps1
+++ josm/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'josm'
   fileType      = 'EXE'
-  url           = 'https://josm.openstreetmap.de/download/windows/josm-setup-18004.exe'
-  checksum      = 'e06420ae216704c93418f0902f766feac1e4301d480c77a11ad9712cae72080b'
+  url           = 'https://josm.openstreetmap.de/download/windows/josm-setup-18118.exe'
+  checksum      = 'de693689432c1f676357f3a71e6d6b9226d11927352c72c84feab05e4be12a64'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
