a418463dd0ad4f9436f46474f6bb8840ca9c87bf
Damiaan Peeters <damiaan@umbrace.be>
2021-08-17 13:05:06 +0200

    JOSM 18118

--- josm/tools/chocolateyinstall.ps1
+++ josm/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = 'josm'
-  fileType      = 'EXE'
-  url           = 'https://josm.openstreetmap.de/download/windows/josm-setup-18004.exe'
-  checksum      = 'e06420ae216704c93418f0902f766feac1e4301d480c77a11ad9712cae72080b'
-  checksumType  = 'sha256'
-  silentArgs    = '/S'
-  validExitCodes= @(0)
+  packageName    = 'josm'
+  fileType       = 'EXE'
+  url            = 'https://josm.openstreetmap.de/download/windows/josm-setup-18118.exe'
+  checksum       = 'DE693689432C1F676357F3A71E6D6B9226D11927352C72C84FEAB05E4BE12A64'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
