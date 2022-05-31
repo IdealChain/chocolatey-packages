e22ca06b25ac358f89ee416af76143beb1ae204f
Daniel Achleitner <daniel.achleitner@gmail.com>
2022-05-31 22:31:06 +0200

    JOSM 18463

--- josm/tools/chocolateyinstall.ps1
+++ josm/tools/chocolateyinstall.ps1
@@ -1,23 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-# Transition to new installer: remove old NSIS-based installation
-[array]$uninstallKey = Get-UninstallRegistryKey -SoftwareName 'JOSM *'
-if ($uninstallKey.Count -eq 1) {
-  $uninstallKey | % {
-    $file = "$($_.UninstallString.Trim('"'))"
-    $instDir = "$(Split-Path $file)\"
-
-    Uninstall-ChocolateyPackage -PackageName $($_.DisplayName) -FileType 'EXE' -SilentArgs "/S _?=$instDir" -ValidExitCodes @(0) -File "$file"
-    Remove-Item "$file"
-    Remove-Item "$instDir"
-  }
-}
-
 $packageArgs = @{
   packageName   = 'josm'
   fileType      = 'EXE'
-  url           = 'https://josm.openstreetmap.de/download/windows/josm-setup-18427-java17.exe'
-  checksum      = 'd6417a752aaf45a868c06a1d5a062f00a60a5ab45039e086689bf6fd386d123c'
+  url           = 'https://josm.openstreetmap.de/download/windows/josm-setup-18463-java17.exe'
+  checksum      = '7534c3f53fcc474ee2fd4ae7060a2c6d086df2ded194fa2973d99c8ebbbc4fa0'
   checksumType  = 'sha256'
   silentArgs    = '/qn'
   validExitCodes= @(0)
