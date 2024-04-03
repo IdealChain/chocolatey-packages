46634c439c82eda8dee44877fa699ef502034410
Daniel Achleitner <daniel.achleitner@gmail.com>
2024-04-03 19:54:59 +0200

    Electron Cash 4.4.0

--- electron-cash.install/tools/chocolateyinstall.ps1
+++ electron-cash.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'electron-cash'
   fileType      = 'EXE'
-  url           = 'https://electroncash.org/downloads/4.3.1/win-linux/Electron-Cash-4.3.1-setup.exe'
-  checksum      = '5f063736f0be1ebaa19ab9ecd333aa146d87b517ba15d0453dc9901939abf57b'
+  url           = 'https://electroncash.org/downloads/4.4.0/win-linux/Electron-Cash-4.4.0-setup.exe'
+  checksum      = 'e8275f788a1186618e5a2c1d9b3d529cdd3d8dafc97939be27cd6838c1d81417'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
--- electron-cash.portable/tools/chocolateyInstall.ps1
+++ electron-cash.portable/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $shortcutPath = "$(Join-Path $env:AppData 'Microsoft\Windows\Start Menu\Programs
 $packageArgs = @{
     packageName    = 'electron-cash'
     fileFullPath   = $installFile
-    url            = 'https://electroncash.org/downloads/4.3.1/win-linux/Electron-Cash-4.3.1.exe'
-    checksum       = 'bcd3b120147bb927062102ae48c9774911197e90600884e5b8f98bee218b5c60'
+    url            = 'https://electroncash.org/downloads/4.4.0/win-linux/Electron-Cash-4.4.0.exe'
+    checksum       = '2778ee1c6dcc6784d814191c04fbd9b12f94341b5e63c04b3ba5e242480b38a0'
     checksumType   = 'sha256'
     forceDownload  = $true
 }
